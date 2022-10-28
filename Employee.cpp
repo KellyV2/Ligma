9fbc597fcfb538b8fb837682a8adc3a13fc1a600
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-10-28 14:15:44 +1030

    Add files via upload

---
+++ Employee.cpp
@@ -0,0 +1,20 @@
+#include "Employee.h"
+
+using namespace std;
+
+Employee::Employee(){
+
+}
+
+Employee::Employee(int payRate){
+  this->payRate = payRate;
+  energyLevel = 100;
+}
+
+void Employee::takeABreak(int mins){
+  for (int i = 0; i < mins; i++) {
+    if (energyLevel <= 98) {
+      energyLevel = energyLevel + 2;
+    }
+  }
+}
---
+++ Employee.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Employee{
+ protected:
+  int payRate;
+  float energyLevel;
+ public:
+  Employee();
+  Employee(int payRate);
+  void takeABreak(int mins);
+  virtual void work(int mins) = 0;
+  virtual float pay() = 0;
+  int get_payRate(){return payRate;}
+  float get_energyLevel(){return energyLevel;}
+  void set_payRate(int n_rate){payRate = n_rate;}
+  void set_energyLevel(float n_energy){energyLevel = n_energy;}
+};
