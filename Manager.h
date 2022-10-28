49dff19a5683bf101f266a0bd37af7843acff139
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-10-28 14:48:23 +1030

    Add files via upload

--- Manager.h
+++ Manager.h
@@ -1,2 +1,17 @@
 #pragma once
 #include "Employee.h"
+
+class Manager : public Employee{
+ private:
+  int daysWorked;
+  float hoursWorked, timeCount;
+ public:
+  Manager();
+  Manager(int payRate);
+  float get_hoursWorked(){return timeCount/60;}
+  void set_hoursWorked(float s_hours) {hoursWorked = s_hours;}
+  int get_daysWorked() {return daysWorked;}
+  void set_daysWorked(int s_days) {daysWorked = s_days;}
+  void work(int mins);
+  float pay();
+};
